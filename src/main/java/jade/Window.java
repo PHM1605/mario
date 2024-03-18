ee75336367757630a93c04d129fdb10d849c671b
PHM1605 <PHM1605@gmail.com>
2024-03-19 00:01:26 +0700

    updates mouse listeners

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -1,10 +1,18 @@
 package jade;
 
 import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Window {
   private int width, height;
   private String title;
+  private long glfwWindow;
   private static Window window = null;
 
   private Window() {
@@ -22,5 +30,55 @@ public class Window {
 
   public void run() {
     System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+    init();
+    loop();
+    // Free the memory
+    glfwFreeCallbacks(glfwWindow);
+    glfwDestroyWindow(glfwWindow);
+    // Terminate GLFW and the free error callback
+    glfwTerminate();
+    glfwSetErrorCallback(null).free();
+  }
+
+  public void init() {
+    // Setup an error callback
+    GLFWErrorCallback.createPrint(System.err).set();
+    // Initialize GLFW
+    if (!glfwInit()) {
+      throw new IllegalStateException("Unable to initialize GLFW.");
+    }
+    // Configure GLFW
+    glfwDefaultWindowHints();
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+    glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+    // Create the window
+    glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+    if (glfwWindow == NULL) {
+      throw new IllegalStateException("Failed to create the GLFW window.");
+    }
+    // Set mouse listeners
+    glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+    glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+    glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+    // Set keyboard listener
+
+    // Make the OpenGL context current
+    glfwMakeContextCurrent(glfwWindow);
+    // Enable v-sync
+    glfwSwapInterval(1);
+    // Make the window visible
+    glfwShowWindow(glfwWindow);
+    GL.createCapabilities();
+  }
+
+  public void loop() {
+    while (!glfwWindowShouldClose(glfwWindow)) {
+      // Poll events
+      glfwPollEvents();
+      glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+      glfwSwapBuffers(glfwWindow);
+    }
   }
 }
